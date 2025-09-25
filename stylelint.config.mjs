c20c03cdd57ed28fb6443944c37bd37b8ba82d50
ethangardner <ethan.gardner@gsa.gov>
2025-09-25 17:28:17 -0400

    update stylelint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-standard"],
-  "rules": {
-    "shorthand-property-no-redundant-values": true,
-    "declaration-block-no-shorthand-property-overrides": true,
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "declaration-block-no-redundant-longhand-properties": null,
     "media-feature-range-notation": "prefix",
-    "number-max-precision": 5
+    "no-descending-specificity": null,
+    "no-duplicate-selectors": null,
+    "number-max-precision": 5,
+    "selector-class-pattern":
+      "^([a-z]+\\:)?[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+){0,2}$",
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["**/*.css.ts", "**/*.js", "**/*.ts"],
-      "customSyntax": "postcss-lit"
-    }
-  ]
+      files: ["src/components/**/*.css.ts"],
+      customSyntax: "postcss-lit",
+    },
+  ],
 };
