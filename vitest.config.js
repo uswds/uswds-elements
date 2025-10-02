d47c16e7b4d3196c475d522585d042edd6fad4ae
ethangardner <ethan.gardner@gsa.gov>
2025-10-02 13:55:17 -0400

    do not include playwright files in vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    include: ["src/**/*.spec.{js,ts}", "src/**/*.test.{js,ts}"],
   },
 });
