700886cddac5cf20aa473525ad70cc0342c6a72c
ethangardner <ethan.gardner@gsa.gov>
2025-10-06 17:13:21 -0400

    add plugin to wrap css files in tagged template literal

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import litCss from "vite-plugin-lit-css";
 
 export default defineConfig({
+  plugins: [litCss()],
   resolve: {
     alias: {
       "@uswds/uswds": resolve(__dirname, "node_modules/@uswds/uswds/dist"),
