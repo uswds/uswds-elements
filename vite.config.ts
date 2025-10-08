3fe31854e01405dac3ff506999c1322740f264cd
ethangardner <ethan.gardner@gsa.gov>
2025-10-08 17:20:45 -0400

    add bundlesize config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,24 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import bundlesize, { type Limit } from "vite-plugin-bundlesize";
+
+const entries: Array<{ name: string; path: string; sizeLimit: string }> = [
+  {
+    name: "components/index",
+    path: "src/components/index",
+    sizeLimit: "0.2 kB",
+  },
+  {
+    name: "components/usa-banner",
+    path: "src/components/usa-banner/index.ts",
+    sizeLimit: "10 kB",
+  },
+  {
+    name: "components/usa-link",
+    path: "src/components/usa-link/index.js",
+    sizeLimit: "0.8 kB",
+  },
+];
 
 export default defineConfig({
   resolve: {
@@ -15,16 +34,33 @@ export default defineConfig({
     },
   },
   build: {
+    sourcemap: "hidden",
     lib: {
-      entry: {
-        "components/usa-banner": "src/components/usa-banner/index.ts",
-        "components/usa-link": "src/components/usa-link/index.js",
-        "components/index": "src/components/index",
-      },
+      entry: entries.reduce(
+        (acc, entry) => {
+          acc[entry.name] = entry.path;
+          return acc;
+        },
+        {} as Record<string, string>,
+      ),
     },
     rollupOptions: {
       external: ["lit"],
       output: { globals: { lit: "lit" }, format: "es" },
     },
   },
+  plugins: [
+    bundlesize({
+      limits: [
+        ...entries.map((item) => {
+          return {
+            name: `${item.name}.js`,
+            limit: item.sizeLimit,
+            mode: "brotli",
+          } as Limit;
+        }),
+        { name: "**/*.cjs", limit: "Infinity" },
+      ],
+    }),
+  ],
 });
