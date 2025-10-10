15ad9d8b1f8d6d98633f764e9183e63b81c78052
ethangardner <ethan.gardner@gsa.gov>
2025-10-10 16:53:36 -0400

    clean up test import

--- internals/build-helpers/index.test.ts
+++ internals/build-helpers/index.test.ts
@@ -1,10 +1,5 @@
 import { describe, it, expect } from "vitest";
-import {
-  mapEntryToLimit,
-  type Entry,
-  mapEntryToTuple,
-  mapEntriesToKeyValue,
-} from "./index";
+import { mapEntryToLimit, type Entry, mapEntriesToKeyValue } from "./index";
 
 describe("mapEntryToLimit", () => {
   it("should correctly transform an entry to a Limit object", () => {
