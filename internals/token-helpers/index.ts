952766598eda9b517c7e06f7c5c4c9ae2defe80c
ethangardner <ethan.gardner@gsa.gov>
2025-10-10 16:15:21 -0400

    extract token naming logic to helper function and clean up style-dictionary config

---
+++ internals/token-helpers/index.ts
@@ -0,0 +1,25 @@
+export const generateTokenName = (token, options) => {
+  if (token.path[0] === "spacing") {
+    return `${options.prefix}-spacing-${token.path[1]}`;
+  }
+
+  if (token.path[0] === "breakpoint") {
+    return `${options.prefix}-breakpoint-${token.path[1]}`;
+  }
+
+  const isFromColorDirectory =
+    token.filePath && token.filePath.includes("tokens/color/");
+
+  if (isFromColorDirectory) {
+    if (
+      token.path.length === 1 &&
+      ["transparent", "black", "white"].includes(token.path[0])
+    ) {
+      return `${options.prefix}-color-${token.path[0]}`;
+    } else {
+      return `${options.prefix}-color-${token.path.join("-")}`;
+    }
+  }
+
+  return `${options.prefix}-${token.path.join("-")}`;
+};
--- style-dictionary.config.js
+++ style-dictionary.config.js
@@ -1,33 +1,10 @@
 import StyleDictionary from "style-dictionary";
+import { generateTokenName } from "./internals/token-helpers/index.ts";
 
 StyleDictionary.registerTransform({
   name: "name/uswds-theme",
   type: "name",
-  transform: function (token, options) {
-    if (token.path[0] === "spacing") {
-      return `${options.prefix}-spacing-${token.path[1]}`;
-    }
-
-    if (token.path[0] === "breakpoint") {
-      return `${options.prefix}-breakpoint-${token.path[1]}`;
-    }
-
-    const isFromColorDirectory =
-      token.filePath && token.filePath.includes("tokens/color/");
-
-    if (isFromColorDirectory) {
-      if (
-        token.path.length === 1 &&
-        ["transparent", "black", "white"].includes(token.path[0])
-      ) {
-        return `${options.prefix}-color-${token.path[0]}`;
-      } else {
-        return `${options.prefix}-color-${token.path.join("-")}`;
-      }
-    }
-
-    return `${options.prefix}-${token.path.join("-")}`;
-  },
+  transform: generateTokenName,
 });
 
 StyleDictionary.registerTransform({
