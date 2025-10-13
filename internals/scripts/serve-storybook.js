3912bff112b8806988e637949d9d5806c1bd9df6
ethangardner <ethan.gardner@gsa.gov>
2025-10-13 11:13:12 -0400

    update path to storybook

--- internals/scripts/serve-storybook.js
+++ internals/scripts/serve-storybook.js
@@ -9,11 +9,9 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log(path.join(__dirname, "..", "storybook-static"));
-
 // Serve static files from storybook-static directory
 app.use(
-  serveStatic(path.join(__dirname, "..", "storybook-static"), {
+  serveStatic(path.join(__dirname, "..", "..", "storybook-static"), {
     index: ["index.html"],
   }),
 );
