d3ce02c173efe0e6f95f4cc781d3d1823cc235c3
ethangardner <ethan.gardner@gsa.gov>
2025-10-17 11:00:57 -0400

    limit custom elements manifest to src/components directory with new config file

---
+++ custom-elements-manifest.config.js
@@ -0,0 +1,8 @@
+export default {
+  globs: ["./src/components/**/*.{js,ts}"],
+  exclude: [
+    "./src/components/**/*.spec.{js,ts}",
+    "./src/components/**/*.stories.{js,ts}",
+  ],
+  litelement: true,
+};
