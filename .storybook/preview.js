f0e270dbf0c23b185184ef73b12a305b381434b4
ethangardner <ethan.gardner@gsa.gov>
2025-11-04 14:56:06 -0500

    restore imports to preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -19,6 +19,14 @@ setStorybookHelpersConfig({
 // Custom theme.
 import UswdsTheme from "./UswdsTheme";
 
+// Global styles.
+import "../storybook/index.css";
+// Documentation overrides.
+import "./index.css";
+
+// USWDS Core props.
+import "../src/core/index.css";
+
 /** @type { import('@storybook/web-components-vite').Preview } */
 const preview = {
   parameters: {
