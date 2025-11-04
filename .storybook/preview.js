bd91e61f397e4fc9eb7acb1488d0cc9e72db6bba
ethangardner <ethan.gardner@gsa.gov>
2025-11-04 16:31:47 -0500

    clean up imports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,28 +4,15 @@
 import { setCustomElementsManifest } from "@storybook/web-components-vite";
 import { setStorybookHelpersConfig } from "@wc-toolkit/storybook-helpers";
 import customElements from "../custom-elements.json";
+import UswdsTheme from "./UswdsTheme";
+import "../storybook/index.css";
+import "./index.css";
+import "../src/core/index.css";
 
 setCustomElementsManifest(customElements);
 setStorybookHelpersConfig({
   hideArgRef: true,
 });
-
-/**
- *
- * StorybookJS theme
- *
- */
-
-// Custom theme.
-import UswdsTheme from "./UswdsTheme";
-
-// Global styles.
-import "../storybook/index.css";
-// Documentation overrides.
-import "./index.css";
-
-// USWDS Core props.
-import "../src/core/index.css";
 
 /** @type { import('@storybook/web-components-vite').Preview } */
 const preview = {
